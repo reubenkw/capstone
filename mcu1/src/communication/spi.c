5306b96bdce8aab71034db707aade1471d7c5640
ElinLiu <elinliu2@gmail.com>
2024-01-27 21:52:15 -0500

    spi rw

---
+++ mcu1/src/communication/spi.c
@@ -0,0 +1,30 @@
+#include "spi.h"
+#include "log.h"
+
+#include <stdio.h>
+
+void Tx(spi_config_t spi_config, uint8_t * tx_buffer, uint8_t len){
+    int spifd=spi_open("/dev/spidev2.0", spi_config);
+    char msg[100];
+    int ret = spi_write(spifd, tx_buffer, len);
+    if (ret < 0) {
+        sprintf(msg, "ERROR: failed to Tx spi msg: %s\n", tx_buffer);
+    } else {
+        sprintf(msg, "INFO: sent %s, to spidev2.0\n ",tx_buffer);
+    }
+    c_log(msg);
+    spi_close(spifd); 
+}
+
+uint8_t * Rx(spi_config_t spi_config, uint8_t * rx_buffer, uint8_t len){
+    int spifd=spi_open("/dev/spidev2.0", spi_config);
+    int ret = spi_read(spifd, rx_buffer, len);
+    if (ret < 0) {
+        c_log("ERROR: failed to Rx spi msg");
+    } else {
+        char msg[100];
+        sprintf(msg, "INFO: read %s, frpm spidev2.0\n ",rx_buffer);
+        c_log(msg);
+    }
+    spi_close(spifd); 
+}
--- mcu1/src/core/main.c
+++ mcu1/src/core/main.c
@@ -1,8 +1,28 @@
 #include "spi.h"
 #include "log.h"
 
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, char** argv)
 {
-    c_log("hello world");
+    c_log("hello world\n");
+
+    spi_config_t spi_config;
+    uint8_t tx_buffer[32];
+    uint8_t rx_buffer[32];
+
+    spi_config.mode=0;
+    spi_config.speed=1000000;
+    spi_config.delay=0;
+    spi_config.bits_per_word=8;
+
+    memset(tx_buffer,0,32);
+    memset(rx_buffer,0,32);
+    sprintf(tx_buffer,"transmitting hello world");
+
+    Tx(spi_config, tx_buffer, strlen(tx_buffer));
+    Rx(spi_config, rx_buffer, strlen(rx_buffer));
+
 	return 0;
 }
