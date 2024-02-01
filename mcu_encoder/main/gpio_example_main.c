22061f91b91e5a149acef1d81a1a10185c02e66f
Elin <elinliu2@gmail.com>
2024-02-01 00:20:35 -0500

    i2c dpio initialization

--- mcu_encoder/main/gpio_example_main.c
+++ mcu_encoder/main/gpio_example_main.c
@@ -10,73 +10,70 @@
 #include <string.h>
 #include <stdlib.h>
 #include <inttypes.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
+
+#include "driver/i2c.h"
 #include "driver/gpio.h"
+#include "freertos/portmacro.h"
 
-/**
- * Brief:
- * This test code shows how to configure gpio and how to use gpio interrupt.
- *
- * GPIO status:
- * GPIO18: output (ESP32C2/ESP32H2 uses GPIO8 as the second output pin)
- * GPIO19: output (ESP32C2/ESP32H2 uses GPIO9 as the second output pin)
- * GPIO4:  input, pulled up, interrupt from rising edge and falling edge
- * GPIO5:  input, pulled up, interrupt from rising edge.
- *
- * Note. These are the default GPIO pins to be used in the example. You can
- * change IO pins in menuconfig.
- *
- * Test:
- * Connect GPIO18(8) with GPIO4
- * Connect GPIO19(9) with GPIO5
- * Generate pulses on GPIO18(8)/19(9), that triggers interrupt on GPIO4/5
- *
- */
+#define DRIVE_LB_A 22
+#define DRIVE_LB_B 23
+#define DRIVE_LB_X 24
 
-#define GPIO_OUTPUT_IO_0    CONFIG_GPIO_OUTPUT_0
-#define GPIO_OUTPUT_IO_1    CONFIG_GPIO_OUTPUT_1
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
-/*
- * Let's say, GPIO_OUTPUT_IO_0=18, GPIO_OUTPUT_IO_1=19
- * In binary representation,
- * 1ULL<<GPIO_OUTPUT_IO_0 is equal to 0000000000000000000001000000000000000000 and
- * 1ULL<<GPIO_OUTPUT_IO_1 is equal to 0000000000000000000010000000000000000000
- * GPIO_OUTPUT_PIN_SEL                0000000000000000000011000000000000000000
- * */
-#define GPIO_INPUT_IO_0     CONFIG_GPIO_INPUT_0
-#define GPIO_INPUT_IO_1     CONFIG_GPIO_INPUT_1
-#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
-/*
- * Let's say, GPIO_INPUT_IO_0=4, GPIO_INPUT_IO_1=5
- * In binary representation,
- * 1ULL<<GPIO_INPUT_IO_0 is equal to 0000000000000000000000000000000000010000 and
- * 1ULL<<GPIO_INPUT_IO_1 is equal to 0000000000000000000000000000000000100000
- * GPIO_INPUT_PIN_SEL                0000000000000000000000000000000000110000
- * */
-#define ESP_INTR_FLAG_DEFAULT 0
+#define DRIVE_RF_A 25
+#define DRIVE_RF_B 28
+#define DRIVE_RF_X 29
 
-static QueueHandle_t gpio_evt_queue = NULL;
+#define DRIVE_RB_A 30
+#define DRIVE_RB_B 31
+#define DRIVE_RB_X 36
 
-static void IRAM_ATTR gpio_isr_handler(void* arg)
-{
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
-}
+#define DRIVE_LF_A 37 
+#define DRIVE_LF_B 38
+#define DRIVE_LF_X 39
 
-static void gpio_task_example(void* arg)
-{
-    uint32_t io_num;
-    for(;;) {
-        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
-        }
-    }
-}
+#define SERVO_X_A 4
+#define SERVO_X_B 5
+#define SERVO_X_X 6
+
+#define SERVO_Y_A 8 
+#define SERVO_Y_B 9 
+#define SERVO_Y_X 10 
+
+#define SERVO_Z_A 19
+#define SERVO_Z_B 20 
+#define SERVO_Z_X 21
+
+#define GPIO_OUTPUT_PIN_SEL  ( \
+    (1ULL<<DRIVE_LB_A) | (1ULL<<DRIVE_LB_B) | (1ULL<<DRIVE_LB_X) | \
+    (1ULL<<DRIVE_LF_A) | (1ULL<<DRIVE_LF_B) | (1ULL<<DRIVE_LF_X) | \
+    (1ULL<<DRIVE_RB_A) | (1ULL<<DRIVE_RB_B) | (1ULL<<DRIVE_RB_X) | \
+    (1ULL<<DRIVE_RF_A) | (1ULL<<DRIVE_RF_B) | (1ULL<<DRIVE_RF_X) | \
+    (1ULL<<SERVO_X_A) | (1ULL<<SERVO_X_B) | (1ULL<<SERVO_X_X) | \
+    (1ULL<<SERVO_Y_A) | (1ULL<<SERVO_Y_B) | (1ULL<<SERVO_Y_X) | \
+    (1ULL<<SERVO_Z_A) | (1ULL<<SERVO_Z_B) | (1ULL<<SERVO_Z_X) \
+)
 
 void app_main(void)
 {
+    // Initialize i2c bus as slave to listen to jetson nano
+    i2c_config_t i2c_jetson_config = {
+        .mode = I2C_MODE_SLAVE, 
+        .sda_io_num = 18, 
+        .scl_io_num = 17, 
+        .sda_pullup_en = true, 
+        .scl_pullup_en = true, 
+        .slave = {
+            .addr_10bit_en = 0, 
+            .slave_addr = 0x2000, 
+            .maximum_speed = 100000
+        }
+    };
+    i2c_param_config(I2C_NUM_0, &i2c_jetson_config);
+    i2c_driver_install(I2C_NUM_0, I2C_MODE_SLAVE,  32, 32, ESP_INTR_FLAG_LOWMED);
+
+    uint8_t encoderVal[7] = {0, 0, 0, 0, 0, 0, 0};
+    i2c_reset_tx_fifo(I2C_NUM_0);
+
     //zero-initialize the config structure.
     gpio_config_t io_conf = {};
     //disable interrupt
@@ -92,43 +89,8 @@ void app_main(void)
     //configure GPIO with the given settings
     gpio_config(&io_conf);
 
-    //interrupt of rising edge
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    //bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    //set as input mode
-    io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
 
-    //change gpio interrupt type for one pin
-    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
-
-    //create a queue to handle gpio event from isr
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //start gpio task
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
-
-    //install gpio isr service
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
-
-    //remove isr handler for gpio number.
-    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin again
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
-
-    printf("Minimum free heap size: %"PRIu32" bytes\n", esp_get_minimum_free_heap_size());
-
-    int cnt = 0;
-    while(1) {
-        printf("cnt: %d\n", cnt++);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
-        gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
+    while(true) {
+        i2c_slave_write_buffer(I2C_NUM_0, encoderVal, 7, portMAX_DELAY);
     }
 }
