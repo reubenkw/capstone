fd718a19758e8b3cd09b4c9f0352046193f505cf
Reuben Walker <reubenwalker42@gmail.com>
2024-02-12 15:55:13 -0500

    generalized points to 2D or 3D

--- jetson/include/core/point.h
+++ jetson/include/core/point.h
@@ -4,16 +4,33 @@
 #include <stdexcept>
 #include <math.h> 
 
-struct Point {
+struct Point2D {
+	double x;
+	double y;
+
+	Point2D(double x = 0, double y = 0) : x(x), y(y) {}
+	bool operator==(Point2D const& rhs) { return x == rhs.x && y == rhs.y; }
+	bool operator!=(Point2D const& rhs) { return !(*this == rhs); }
+	Point2D operator+(Point2D const& rhs) { return Point2D(x + rhs.x, y + rhs.y); }
+	Point2D operator-(Point2D const& rhs) { return Point2D(x - rhs.x, y - rhs.y); }
+	double& operator[](int index) {
+		if (index == 0) return x;
+		else if (index == 1) return y;
+		else throw std::out_of_range("Index out of range");
+	}
+	double mag() { return sqrt(x * x + y * y); }
+};
+
+struct Point3D {
 	double x;
 	double y;
 	double z;
 
-	Point(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
-	bool operator==(Point const& rhs) { return x == rhs.x && y == rhs.y && z == rhs.z; }
-	bool operator!=(Point const& rhs) { return !(*this == rhs); }
-	Point operator+(Point const& rhs) { return Point(x + rhs.x, y + rhs.y, z + rhs.z); }
-	Point operator-(Point const& rhs) { return Point(x - rhs.x, y - rhs.y, z - rhs.z); }
+	Point3D(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
+	bool operator==(Point3D const& rhs) { return x == rhs.x && y == rhs.y && z == rhs.z; }
+	bool operator!=(Point3D const& rhs) { return !(*this == rhs); }
+	Point3D operator+(Point3D const& rhs) { return Point3D(x + rhs.x, y + rhs.y, z + rhs.z); }
+	Point3D operator-(Point3D const& rhs) { return Point3D(x - rhs.x, y - rhs.y, z - rhs.z); }
 	double& operator[](int index) {
 		if (index == 0) return x;
 		else if (index == 1) return y;
