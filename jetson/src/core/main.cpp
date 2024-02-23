a43e53ebf897a3ffe33a4c806d0d4fd918252648
reubenkw <reubenwalker42@gmail.com>
2024-02-23 17:36:00 -0500

    reading i2c test

--- jetson/src/core/main.cpp
+++ jetson/src/core/main.cpp
@@ -76,9 +76,9 @@ void test_camera_image() {
 void test_i2c() {
 	int i2c_bus_file = open_i2c();
 	log(std::string("INFO: bus file: ") + std::to_string(i2c_bus_file));
-	uint8_t data[2] = {0, 0};
+	uint8_t data[4] = {0, 0, 0, 0};
 	for (int i = 0; i < 10; i++){
-		read_i2c(i2c_bus_file, MCU_ENCODER, (uint8_t * )data, 2);
+		read_i2c(i2c_bus_file, 0x42, (uint8_t * )data, 4);
 	}
 }
 
