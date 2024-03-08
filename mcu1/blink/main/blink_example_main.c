a171b56a3e8348d44bd10c95403640ea7510a67a
Elin <elinliu2@gmail.com>
2024-03-07 22:47:41 -0500

    fix logging

--- mcu1/blink/main/blink_example_main.c
+++ mcu1/blink/main/blink_example_main.c
@@ -7,6 +7,7 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <unistd.h>
 
 #include "driver/i2c.h"
 #include "driver/spi_common.h"
@@ -14,9 +15,18 @@
 #include "freertos/portmacro.h"
 #include "led_strip.h"
 #include "esp_log.h"
-#include <unistd.h>
+#include "led_strip.h"
 
 #include "constants.h"
+
+// LED for debugging
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+static led_strip_handle_t led_strip;
+
+void led_set_color(int r, int g, int b){
+    led_strip_set_pixel(led_strip, 0, r, g, b);
+    led_strip_refresh(led_strip);
+}
 
 #define WRITE 0
 #define READ 1
@@ -36,7 +46,6 @@
 #define LED_1 15
 #define LED_2 16
 #define LED_3 17
-#define LED_DEV 2
 
 #define GPIO_LED_PIN_SEL ( (1ULL<<LED_1) | (1ULL<<LED_2) |(1ULL<<LED_3) )
 
@@ -324,44 +333,79 @@ void test_stepper() {
     }
 }
 
-void final_main() {
-    // initialize_led();
-    // gpio_set_level(LED_1, 1);
+static void configure_led(void)
+{
+    /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1, // at least one LED on board
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    /* Set all LED off to clear all pixels */
+    led_strip_clear(led_strip);
+}
 
-    // // Initialize spi bus as master
-    // init_boost();
-    // initialize_spi();
-
-    // // Add DC motor controller as spi device
-    // spi_device_interface_config_t dc_mc_config = {
-    //     .command_bits = 2, 
-    //     .address_bits = 6, 
-    //     .mode = 0,
-    //     .clock_speed_hz = (20 * 1000 * 1000), 
-    //     .queue_size = 1,
-    //     .spics_io_num = 18
-    // }; 
-    // spi_device_handle_t dc_mc_spi;
-    // spi_bus_add_device(SPI_HOST, &dc_mc_config, &dc_mc_spi);
-
-    // // Add stepper motor controller as spi device
-    // spi_device_interface_config_t stp_mc_config = {
-    //     .command_bits = 2, 
-    //     .address_bits = 6, 
-    //     .mode = 0,
-    //     .clock_speed_hz = (20 * 1000 * 1000), 
-    //     .queue_size = 1,
-    //     .spics_io_num = 35
-    // }; 
-    // spi_device_handle_t stp_mc_spi;
-    // spi_bus_add_device(SPI_HOST, &stp_mc_config, &stp_mc_spi);
-    
-    // // Initialize i2c bus as slave to listen to jetson nano
-    // initialize_limit_gpio();
+void test_jetson_ctrl() {
+    configure_led();
+    led_set_color(50, 0, 0);
     initialize_i2c_jetson();
 
-    // printf("mcu initialized"); 
-    // gpio_set_level(LED_2, 1);
+    uint8_t rx_data[DATA_LENGTH + 1] = {0};
+    while(true){
+        // wait until jetson nano reads from mcu1 over i2c 
+        // based on jetson nano command, do different tasks
+        i2c_slave_read_buffer(I2C_HOST, rx_data, DATA_LENGTH + 1, portMAX_DELAY);
+        printf("read from jetson: %d %d %d %d", rx_data[1], rx_data[2], rx_data[3], rx_data[4]); 
+        switch (rx_data[ADDR_INDEX]) {
+            case LED:
+                led_set_color(0, 50, 0);
+            break;
+        }
+    }
+}
+
+void final_main() {
+
+    initialize_led();
+    gpio_set_level(LED_1, 1);
+
+    // Initialize spi bus as master
+    init_boost();
+    initialize_spi();
+
+    // Add DC motor controller as spi device
+    spi_device_interface_config_t dc_mc_config = {
+        .command_bits = 2, 
+        .address_bits = 6, 
+        .mode = 0,
+        .clock_speed_hz = (20 * 1000 * 1000), 
+        .queue_size = 1,
+        .spics_io_num = 18
+    }; 
+    spi_device_handle_t dc_mc_spi;
+    spi_bus_add_device(SPI_HOST, &dc_mc_config, &dc_mc_spi);
+
+    // Add stepper motor controller as spi device
+    spi_device_interface_config_t stp_mc_config = {
+        .command_bits = 2, 
+        .address_bits = 6, 
+        .mode = 0,
+        .clock_speed_hz = (20 * 1000 * 1000), 
+        .queue_size = 1,
+        .spics_io_num = 35
+    }; 
+    spi_device_handle_t stp_mc_spi;
+    spi_bus_add_device(SPI_HOST, &stp_mc_config, &stp_mc_spi);
+    
+    // Initialize i2c bus as slave to listen to jetson nano
+    initialize_limit_gpio();
+    initialize_i2c_jetson();
+
+    printf("mcu initialized"); 
+    gpio_set_level(LED_2, 1);
 
     // ignore initial byte
     uint8_t rx_data[DATA_LENGTH + 1] = {0};
@@ -371,56 +415,40 @@ void final_main() {
         i2c_slave_read_buffer(I2C_HOST, rx_data, DATA_LENGTH + 1, portMAX_DELAY);
         printf("read from jetson: %d %d %d %d", rx_data[1], rx_data[2], rx_data[3], rx_data[4]); 
         switch (rx_data[ADDR_INDEX]) {
-            // case DRIVE_MC: 
-            //     write_spi(dc_mc_spi, rx_data[REG_INDEX], &rx_data[DATA_INDEX]);
-            // break;
-            // case SERVO_MC:
-            //     printf("servo_mc"); 
-            //     uint32_t delay = 10;
-            //     if ((rx_data[CMD_INDEX] & 1) == GO_CMD){
-            //         while(i2c_slave_read_buffer(I2C_HOST, rx_data, DATA_LENGTH + 1, delay) == 0){
-            //             stepper_rotation(OP_CTRL_1);
-            //         }
-            //     } else if (((rx_data[CMD_INDEX] & 0b10) >> 1) == GO_CMD) {
-            //         while(i2c_slave_read_buffer(I2C_HOST, rx_data, DATA_LENGTH + 1, delay) == 0){
-            //             stepper_rotation(OP_CTRL_2);
-            //         }
-            //     } else if (((rx_data[CMD_INDEX] & 0b100) >> 1) == GO_CMD) {
-            //         while(i2c_slave_read_buffer(I2C_HOST, rx_data, DATA_LENGTH + 1, delay) == 0){
-            //             stepper_rotation(OP_CTRL_3);
-            //         }
-            //     }
-            // break;
-            case LED:
-                gpio_config_t io_conf = {};
-                //disable interrupt
-                io_conf.intr_type = GPIO_INTR_DISABLE;
-                //set as input mode
-                io_conf.mode = GPIO_MODE_OUTPUT;
-                //bit mask of the pins that you want to set,e.g.GPIO18/19
-                io_conf.pin_bit_mask = (1<<LED_DEV);
-                //disable pull-down mode
-                io_conf.pull_down_en = 1;
-                //disable pull-up mode
-                io_conf.pull_up_en = 0;
-                //configure GPIO with the given settings
-                gpio_config(&io_conf);
-                gpio_set_level(LED_DEV, 1);
+            case DRIVE_MC: 
+                write_spi(dc_mc_spi, rx_data[REG_INDEX], &rx_data[DATA_INDEX]);
+            break;
+            case SERVO_MC:
+                printf("servo_mc"); 
+                uint32_t delay = 10;
+                if ((rx_data[CMD_INDEX] & 1) == GO_CMD){
+                    while(i2c_slave_read_buffer(I2C_HOST, rx_data, DATA_LENGTH + 1, delay) == 0){
+                        stepper_rotation(OP_CTRL_1);
+                    }
+                } else if (((rx_data[CMD_INDEX] & 0b10) >> 1) == GO_CMD) {
+                    while(i2c_slave_read_buffer(I2C_HOST, rx_data, DATA_LENGTH + 1, delay) == 0){
+                        stepper_rotation(OP_CTRL_2);
+                    }
+                } else if (((rx_data[CMD_INDEX] & 0b100) >> 1) == GO_CMD) {
+                    while(i2c_slave_read_buffer(I2C_HOST, rx_data, DATA_LENGTH + 1, delay) == 0){
+                        stepper_rotation(OP_CTRL_3);
+                    }
+                }
             break;
         }
-        // uint8_t limitVal = 0;
-        // for(uint8_t i = 0; i < sizeof(limit_pins)/sizeof(limit_pins[0]); i++){
-        //     uint8_t level = gpio_get_level(limit_pins[i]);
-        //     limitVal = limitVal | (level << i);
-        // }
-        // // need to pad an extra zero
-        // uint8_t tx_data[2] = {limitVal, 0x0};
-        // printf("jetson write: %d", limitVal); 
-        // i2c_slave_write_buffer(I2C_HOST, tx_data, 2, portMAX_DELAY);
+        uint8_t limitVal = 0;
+        for(uint8_t i = 0; i < sizeof(limit_pins)/sizeof(limit_pins[0]); i++){
+            uint8_t level = gpio_get_level(limit_pins[i]);
+            limitVal = limitVal | (level << i);
+        }
+        // need to pad an extra zero
+        uint8_t tx_data[2] = {limitVal, 0x0};
+        printf("jetson write: %d", limitVal); 
+        i2c_slave_write_buffer(I2C_HOST, tx_data, 2, portMAX_DELAY);
     }
 }
 
 void app_main(void)
 {   
-    test_stepper();
+    test_jetson_ctrl();
 }
